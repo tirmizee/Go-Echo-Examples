b199f5319dd67c11c5860362f636a6ed2b5986f3
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2022-01-09 17:17:11 +0700

    init project

---
+++ Echo-HTTP-Method/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+// Handler
+
+func HandlerGET(c echo.Context) error {
+	return c.String(http.StatusOK, "GET Method")
+}
+
+func HandlerPOST(c echo.Context) error {
+	return c.String(http.StatusOK, "POST Method")
+}
+
+func HandlerPUT(c echo.Context) error {
+	return c.String(http.StatusOK, "PUT Method")
+}
+
+func HandlerDELETE(c echo.Context) error {
+	return c.String(http.StatusOK, "DELETE Method")
+}
+
+func main() {
+
+	e := echo.New()
+
+	e.GET("/GET", HandlerGET)
+	e.PUT("/PUT", HandlerPUT)
+	e.GET("/POST", HandlerPOST)
+	e.GET("/DELETE", HandlerDELETE)
+
+	e.Logger.Fatal(e.Start(":8080"))
+
+}
